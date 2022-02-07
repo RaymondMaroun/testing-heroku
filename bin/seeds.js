c2fddc80cc3215f2534f198ea068f0f4579f174f
RayMaroun <raymond.maroun@ironhack.com>
2022-02-07 20:27:51 +0200

    fix

--- bin/seeds.js
+++ bin/seeds.js
@@ -5,12 +5,16 @@ const Book = require('../models/Book.model');
 
 const MONGO_URI = process.env.MONGODB_URI || 'mongodb://localhost/library-project';
 
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true
-});
+mongoose
+  .connect(MONGO_URI)
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  });
 
 // PASTE HERE THE LIST OF BOOKS PROVIDED IN THIS GIST: https://gist.github.com/ironhack-edu/2816267a015d4870f95275cb873d33b6
 
